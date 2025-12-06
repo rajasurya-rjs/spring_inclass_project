50de4c28936a85a6714a230929c3b8f2b276b408
Rajasurya <rajasurya2006rjs@gmail.com>
2025-12-06 12:38:44 +0530

    auth controller almost

---
+++ src/main/java/org/example/rideshare/controller/AuthController.java
@@ -0,0 +1,31 @@
+package org.example.rideshare.controller;
+
+import org.example.rideshare.dto.AuthRequest;
+import org.example.rideshare.dto.UserRegisterRequest;
+import org.example.rideshare.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService service;
+
+    public AuthController(AuthService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserRegisterRequest req){
+        service.register(req);
+        return ResponseEntity.ok("Registered");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest req){
+        String token = service.login(req);
+        if(token == null) return ResponseEntity.status(401).body("Invalid");
+        return ResponseEntity.ok(token);
+    }
+}
